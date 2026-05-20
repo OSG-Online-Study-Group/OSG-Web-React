5fd30c5cd0d3306838032748ad4465c15e1d5e00
juanoliveira0020 <juanoliveira200741@gmail.com>
2026-05-20 16:45:16 -0300

    refactor: update Router structure and maintain basename for routing

--- src/App.jsx
+++ src/App.jsx
@@ -20,26 +20,30 @@ import { Grupos } from "./components/grupos";
 export default function App() {
   return (
     <div className="container-app">
-     <Router>
-      <Routes>
-        <Route path="/" element={<Abertura />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Cadastro" element={<Cadastro />} />
-        <Route path="/EscolhaMaterias" element={<EscolhaMaterias />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/DuelosPendentes" element={<DuelosPendentes />} />
-        <Route path="/Grupos" element={<Grupos />} />
-        <Route path="/GrupoQuimica" element={<GrupoQuimica />} />
-        <Route path="/GrupoEconomia" element={<GrupoEconomia />} />
-        <Route path="/Game" element={<Game />} />
-        <Route path="/ConviteDuelo" element={<ConviteDuelo />} />
-        <Route path="/Treino" element={<Treino />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/Mensagem" element={<Mensagem />} />
-        <Route path="/Ranking" element={<Ranking />} />
-        <Route path="/RankingUser" element={<RankingUser />} />
-      </Routes>
-    </Router>
+
+      {/* ADICIONE APENAS O BASENAME */}
+      <Router basename="/OSG-Web-React">
+
+        <Routes>
+          <Route path="/" element={<Abertura />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Cadastro" element={<Cadastro />} />
+          <Route path="/EscolhaMaterias" element={<EscolhaMaterias />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/DuelosPendentes" element={<DuelosPendentes />} />
+          <Route path="/Grupos" element={<Grupos />} />
+          <Route path="/GrupoQuimica" element={<GrupoQuimica />} />
+          <Route path="/GrupoEconomia" element={<GrupoEconomia />} />
+          <Route path="/Game" element={<Game />} />
+          <Route path="/ConviteDuelo" element={<ConviteDuelo />} />
+          <Route path="/Treino" element={<Treino />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Mensagem" element={<Mensagem />} />
+          <Route path="/Ranking" element={<Ranking />} />
+          <Route path="/RankingUser" element={<RankingUser />} />
+        </Routes>
+
+      </Router>
     </div>
   );
 }
